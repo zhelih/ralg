322e586716c5b77256e6037fa375833775f15b4d
Eugene Lykhovyd <lykhovyd@gmail.com>
2023-08-03 21:53:08 +0000

    stress tests

--- ocaml/test.ml
+++ ocaml/test.ml
@@ -2,10 +2,15 @@ open Ralg
 
 let ralg_options = default_options ()
 
+let sign = function x when x < 0. -> -1. | _ -> 1.
+
+let run_ralg f_obj f_grad x0 =
+  let f_cb x = Some ( (f_obj x), (f_grad x) ) in
+  ralg ralg_options f_cb x0
+
 let test_abs() =
   let x0 = Float.Array.make 2 1000. in
 
-  let sign = function x when x < 0. -> -1. | _ -> 1. in
   let f_obj x =
     let x0 = Float.Array.get x 0 in
     let x1 = Float.Array.get x 1 in
@@ -20,12 +25,37 @@ let test_abs() =
     Float.Array.set res 1 g1;
     res
   in
-  let f_cb x = Some ( (f_obj x), (f_grad x) ) in
-  let opt_obj, opt_x = ralg ralg_options f_cb x0 in
+  let opt_obj, opt_x = run_ralg f_obj f_grad x0 in
   Printf.printf "OCaml side: optimal objective %f, optimal point is %f, %f\n" opt_obj (Float.Array.get opt_x 0) (Float.Array.get opt_x 1);
   if abs_float (opt_obj -. 5.) < 1.e-10 then
     Printf.printf "TEST OK\n"
   else
     Printf.printf "----------- TEST FAILED at %f ----------\n" opt_obj
 
-let () = test_abs()
+let test_stress () =
+  (* this is a high dimension test with a very quick callback and large dimendsions to test latecncy between OCaml stubs and direct C++ run *)
+  let dims = 30 in
+  let x0 = Float.Array.make dims 1000. in
+
+  let f_obj x =
+    snd @@ Float.Array.fold_left (fun (i,prev) xi -> i+.1., prev +. i*.(abs_float (xi -. i))) (1., 0.) x
+  in
+  let f_grad x =
+    Float.Array.init dims (fun i ->
+      let fi = 1. +. float i in
+      let xi = Float.Array.get x i in
+      fi *. (sign ( (xi -. fi) ) )
+    )
+  in
+  let opt_obj, opt_x = run_ralg f_obj f_grad x0 in
+  Printf.printf "STRESS test Objective %f\n" opt_obj;
+  Float.Array.iteri (fun i x ->
+    Printf.printf "solution x %d = %f\n" i x
+  ) opt_x;
+  ()
+
+
+let () =
+  let () = test_abs () in
+  let () = test_stress () in
+  ()
--- test.cpp
+++ test.cpp
@@ -74,5 +74,25 @@ int main()
           RALG_MIN);
   printf("for |x-3| + 100|x+2| + 0.001|y| -> min the answer is %.2lf %.2lf\n", res[0], res[1]);
 
+  // stress test
+  int dim = 100;
+  double* x0_stress = new double[dim];
+  double* res_stress = new double[dim];
+  for(int i = 0; i < dim; ++i) x0_stress[i] = 1000.;
+  ralg(&opt,
+          [dim](const double* x, double& f, double* grad) -> bool
+          {
+            f = 0.;
+            for(int i = 0; i < dim; ++i) { f += (i+1.) * fabs(x[i] - i - 1); }
+            for(int i = 0; i < dim; ++i) { grad[i] = (i+1)*sign(x[i]-i-1);}
+            return true;
+          },
+          dim,
+          x0_stress,
+          res_stress,
+          RALG_MIN);
+  delete [] res_stress;
+  delete [] x0_stress;
+
   return 0;
 }
